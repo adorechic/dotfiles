a7bce13e9ec0f838a510e9708419bc1a5e208c9b
adorechic <adorechic@gmail.com>
2013-07-20 11:46:39 +0900

    Add mode-line view

--- home/.emacs.d/conf/05-view.el
+++ home/.emacs.d/conf/05-view.el
@@ -2,5 +2,25 @@
 (setq hl-line-face 'underline) ; 下線
 (global-hl-line-mode)
 
-;; カラム番号をモードラインに表示
+;;;; mode line
+;; カラム番号
 (column-number-mode t)
+
+;; ファイルサイズ
+(size-indication-mode t)
+
+;; 時計
+(setq display-time-day-and-date t) ; 曜日・月・日を表示
+(display-time-mode t)
+
+;; リージョン内の行数と文字数（範囲指定時）
+(defun count-lines-and-chars ()
+  (if mark-active
+      (format "%d lines,%d chars "
+              (count-lines (region-beginning) (region-end))
+              (- (region-end) (region-beginning)))
+      ;;(count-lines-region (region-beginning) (region-end)) ;; これだとエコーエリアがチラつく
+    ""))
+
+(add-to-list 'default-mode-line-format
+             '(:eval (count-lines-and-chars)))
