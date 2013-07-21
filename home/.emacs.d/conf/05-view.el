cf3b60758ed298cc4dc14e2ebce9361a3fa79f9f
adorechic <adorechic@gmail.com>
2013-07-21 18:51:54 +0900

    Add brace highlight

--- home/.emacs.d/conf/05-view.el
+++ home/.emacs.d/conf/05-view.el
@@ -27,3 +27,12 @@
 
 (add-to-list 'default-mode-line-format
              '(:eval (count-lines-and-chars)))
+
+;; paren-mode: 対応する括弧を強調して表示する
+(setq show-paren-delay 0) ; 表示までの秒数。初期値は0.125
+(show-paren-mode t) ; 有効化
+;; parenのスタイル: expressionは括弧内も強調表示
+(setq show-paren-style 'expression)
+;; フェイスを変更する
+(set-face-background 'show-paren-match-face nil)
+(set-face-underline-p 'show-paren-match-face "yellow")
