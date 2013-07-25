3f0fd4cd22992b8ecc3f3253d9da8aeb5e180439
adorechic <adorechic@gmail.com>
2013-07-26 00:47:52 +0900

    Add dict and snippet

--- home/.emacs.d/conf/10-auto-complete.el
+++ home/.emacs.d/conf/10-auto-complete.el
@@ -2,3 +2,5 @@
 (require 'auto-complete-config)
 (ac-config-default)
 (add-to-list 'ac-modes 'text-mode) ;; text-modeでも自動的に有効にする
+
+(add-to-list 'ac-user-dictionary-files '"~/.emacs.d/ac-dict/dict")
