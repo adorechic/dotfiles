cdaa380da5232c10463c95da4362a46fdadf567b
adorechic <adorechic@gmail.com>
2015-08-26 02:12:43 +0900

    Complement via TAB

--- home/.emacs.d/conf/10-helm.el
+++ home/.emacs.d/conf/10-helm.el
@@ -1,3 +1,8 @@
 (require 'helm)
 (helm-mode 1)
 (define-key global-map (kbd "C-x C-o") 'helm-mini)
+
+;; TABで補完
+(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+;; For helm-find-files etc.
+(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
