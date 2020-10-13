4552d45e55c95788ecf1429173ab7fbbd8cabad3
adorechic <adorechic@gmail.com>
2020-10-13 13:42:11 +0900

    Improve php

--- home/.emacs.d/conf/30-php-mode.el
+++ home/.emacs.d/conf/30-php-mode.el
@@ -1,3 +1,9 @@
 ;; php-mode(
 (require 'php-mode)
 (add-to-list 'auto-mode-alist '(".inc$" . php-mode))
+(add-hook 'php-mode-hook
+  '(lambda()
+     (setq tab-width 4)
+     (setq indent-tabs-mode t)
+     (setq c-basic-offset 4)
+   ))
