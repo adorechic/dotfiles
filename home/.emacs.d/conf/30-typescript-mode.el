d538b6468882806a75a06079bf0222bb24d84e93
adorechic <adorechic@gmail.com>
2020-10-13 13:42:57 +0900

    Add tsx

--- home/.emacs.d/conf/30-typescript-mode.el
+++ home/.emacs.d/conf/30-typescript-mode.el
@@ -1,5 +1,6 @@
 (require 'typescript-mode)
 (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode))
 
 (require 'tide)
 (add-hook 'typescript-mode-hook
